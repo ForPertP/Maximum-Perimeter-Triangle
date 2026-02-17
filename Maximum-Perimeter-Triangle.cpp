6ee1ac5b375c75dce49ac7464afde1a53e9b9d41
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-02-18 02:26:16 +0900

    Update Maximum-Perimeter-Triangle.cpp

--- Maximum-Perimeter-Triangle.cpp
+++ Maximum-Perimeter-Triangle.cpp
@@ -1,1 +1,41 @@
-Maximum-Perimeter-Triangle
+string ltrim(const string &str)
+{
+    string s(str);
+    s.erase(
+        s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
+    );
+    return s;
+}
+
+std::vector<string> split(const string &str)
+{
+    vector<string> tokens;
+    string_view str_view(str);
+    size_t start = 0;
+    size_t end = 0;
+
+    while ((end = str_view.find(' ', start)) != string_view::npos)
+    {
+        if (end > start)
+        {
+            tokens.emplace_back(str_view.substr(start, end - start));
+        }
+        start = end + 1;
+    }
+
+    if (start < str_view.size())
+    {
+        tokens.emplace_back(str_view.substr(start));
+    }
+
+    return tokens;
+}
