854685d4bb4c0df96d7f872c182ef42fd5bf2212
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-02-20 01:50:50 +0900

    Update Maximum-Perimeter-Triangle.cs

--- Maximum-Perimeter-Triangle.cs
+++ Maximum-Perimeter-Triangle.cs
@@ -37,3 +37,23 @@ class Result
         return new List<int> { -1 };
     }
 }
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> sticks = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sticksTemp => Convert.ToInt32(sticksTemp)).ToList();
+
+        List<int> result = Result.maximumPerimeterTriangle(sticks);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
