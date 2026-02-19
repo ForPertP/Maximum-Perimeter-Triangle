c0374c3607691ae5661782d1cf11179370c131af
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-02-20 01:50:09 +0900

    Add maximumPerimeterTriangle function implementation
    Implement maximumPerimeterTriangle function to find the maximum perimeter triangle from a list of sticks.

--- Maximum-Perimeter-Triangle.cs
+++ Maximum-Perimeter-Triangle.cs
@@ -1,1 +1,39 @@
-Maximum-Perimeter-Triangle
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    /*
+     * Complete the 'maximumPerimeterTriangle' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY sticks as parameter.
+     */
+
+    public static List<int> maximumPerimeterTriangle(List<int> sticks)
+    {
+        if (sticks.Count < 3)
+            return new List<int> { -1 };
+
+        sticks.Sort();
+
+        for (int i = sticks.Count - 1; i >= 2; i--)
+        {
+            if (sticks[i - 2] + sticks[i - 1] > sticks[i])
+                return new List<int> { sticks[i - 2], sticks[i - 1], sticks[i] };
+        }
+
+        return new List<int> { -1 };
+    }
+}
