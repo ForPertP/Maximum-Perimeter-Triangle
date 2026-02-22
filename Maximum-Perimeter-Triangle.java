066ece06d6b1934ae346284e925f4917ddb23819
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-02-23 01:53:08 +0900

    Update Maximum-Perimeter-Triangle.java

--- Maximum-Perimeter-Triangle.java
+++ Maximum-Perimeter-Triangle.java
@@ -10,9 +10,7 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-class Result
-{
-
+class Result {
     /*
      * Complete the 'maximumPerimeterTriangle' function below.
      *
